158727c4360716e2d16618249bcb60a191facace
JPTokyo <anomyuslol@gmail.com>
2026-07-09 21:51:03 +0200

    feat: add three Codewars tasks

--- codeWarsTasks.js
+++ codeWarsTasks.js
@@ -15,3 +15,47 @@ function makeNegative(num) {
     return num;
   }
 }
+// Convert boolean values to strings 'Yes' or 'No'.
+function boolToWord(bool) {
+  if (bool === true) {
+    return "Yes";
+  } else {
+    return "No";
+  }
+}
+
+// Basic Mathematical Operations
+function basicOp(operation, value1, value2) {
+  switch (operation) {
+    case "+":
+      return value1 + value2;
+      break;
+
+    case "-":
+      return value1 - value2;
+      break;
+
+    case "*":
+      return value1 * value2;
+      break;
+
+    case "/":
+      return value1 / value2;
+      break;
+  }
+
+  return 0;
+}
+
+// Opposite number
+function opposite(number) {
+  if (number > 0) {
+    return -number;
+  } else {
+    return (number * -1);
+  }
+}
+// Simpler solution
+function opposite(number) {
+  return -number;
+}
