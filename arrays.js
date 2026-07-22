49b93c2ad5204f317fe0e2927a838209dd499f88
sseraflm <anomyuslol@gmail.com>
2026-07-23 00:41:50 +0200

    feat: add array methods practice

--- arrays.js
+++ arrays.js
@@ -34,3 +34,34 @@ function removeLastSong(songs) {
 const removedSong = removeLastSong(songs);
 
 console.log(removedSong);
+
+// Array methods
+
+let numbers = [4, 7, 10, 13, 18, 21];
+
+let doubled = numbers.map(number => number * 2);
+console.log(doubled);
+
+const evenNumbers = numbers.filter(number => number % 2 === 0);
+console.log(evenNumbers);
+
+const firstNumberOver15 = numbers.find(number => number > 15);
+console.log(firstNumberOver15);
+
+const hasNumberOver20 = numbers.some(number => number > 20);
+console.log(hasNumberOver20);
+
+const areAllNumbersPositive = numbers.every(number => number > 0);
+console.log(areAllNumbersPositive);
+
+const upperCaseSongs = songs.map(song => song.toUpperCase());
+console.log(upperCaseSongs);
+
+const longSongs = songs.filter(song => song.length > 5);
+console.log(longSongs);
+
+const showSong = songs.find(song => song === "Show");
+console.log(showSong);
+
+const atleastTen = songs.some(song => song.length > 10);
+console.log(atleastTen);
