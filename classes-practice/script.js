d6885179bab0b8492028bcfad6eecc35a567fa92
sseraflm <anomyuslol@gmail.com>
2026-08-25 00:56:11 +0200

    feat: add initial classes practice

---
+++ classes-practice/script.js
@@ -0,0 +1,39 @@
+class Book {
+
+    constructor(title, releaseYear, price) {
+        this.title = title;
+        this.releaseYear = releaseYear;
+        this.price = price;
+        this.isRead = false;
+    }
+
+    getBookInformation() {
+        return `Book title: ${this.title}, Book release year: ${this.releaseYear}`;
+    }
+
+    readTheBook() {
+        this.isRead = true;
+    }
+
+    get title() {
+        return this._title;
+    
+    }
+
+    get discountPrice() {
+        return this.price * 0.80;
+    }
+
+    set title(value) {
+        if (value.length < 1) {
+            console.log("The title cannot be empty.")
+            return
+        }
+        this._title = value;
+    }
+
+}
+
+let book = new Book("Witcher", 1993);
+console.log(book.title, book.releaseYear)
+
