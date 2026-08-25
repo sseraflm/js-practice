6ed70b167ef7b6c4c2cc42f122ec7e6ffd9b940c
sseraflm <anomyuslol@gmail.com>
2026-08-26 00:47:03 +0200

    feat: add class Inheritance practice

--- classes-practice/script.js
+++ classes-practice/script.js
@@ -34,6 +34,33 @@ class Book {
 
 }
 
-let book = new Book("Witcher", 1993);
+const book = new Book("Witcher", 1993);
 console.log(book.title, book.releaseYear)
 
+// Inheritance
+
+class Animal {
+    constructor(name) {
+        this.name = name;
+    }
+
+    showAnimal() {
+        console.log(`the animal name is ${this.name}`)
+    }
+}
+
+class Fox extends Animal {
+    constructor(name, species) {
+        super(name);
+        this.species = species;
+    }
+    showAnimal() {
+    super.showAnimal();
+    console.log(`The animal species is: ${this.species}`);
+
+    }
+}
+
+const fox = new Fox("Yuki", "Arctic Fox");
+
+fox.showAnimal();
