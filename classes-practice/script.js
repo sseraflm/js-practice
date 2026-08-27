4c4ea84a96a20c0ab52079c1bcce37251c64145e
sseraflm <anomyuslol@gmail.com>
2026-08-27 22:06:02 +0200

    feat: add static and private class practice

--- classes-practice/script.js
+++ classes-practice/script.js
@@ -64,3 +64,24 @@ class Fox extends Animal {
 const fox = new Fox("Yuki", "Arctic Fox");
 
 fox.showAnimal();
+
+// Static, Private
+
+class BankAccount {
+    static totalAccounts = 0;
+
+    #balance;
+
+    constructor(balance) {
+        this.#balance = balance;
+        BankAccount.totalAccounts++;
+    }
+
+    getBalance() {
+        return this.#balance;
+    }
+
+    static create(balance) {
+        return new BankAccount(balance)
+    }
+}
