77793bee3b9c538cef9ce11fa0a9ad16ccd78366
sseraflm <anomyuslol@gmail.com>
2026-09-04 20:19:48 +0200

    feat: add debounce practice

--- closures-event-loop/closures-practice.js
+++ closures-event-loop/closures-practice.js
@@ -1,54 +1,54 @@
-// function createCounter() {
-//   let count = 0;
+function createCounter() {
+  let count = 0;
 
-//   function increment() {
-//     return count++;
-//   }
+  function increment() {
+    return count++;
+  }
 
-//   function decrement() {
-//     return count--;
-//   }
+  function decrement() {
+    return count--;
+  }
 
-//   function getValue() {
-//     return count;
-//   }
+  function getValue() {
+    return count;
+  }
 
-//   return {
-//     increment: increment,
-//     decrement: decrement,
-//     getValue: getValue,
-//   };
-// }
+  return {
+    increment: increment,
+    decrement: decrement,
+    getValue: getValue,
+  };
+}
 
-// const counter1 = createCounter();
-// console.log(counter1.increment());
-// console.log(counter1.increment());
-// console.log(counter1.getValue());
+const counter1 = createCounter();
+console.log(counter1.increment());
+console.log(counter1.increment());
+console.log(counter1.getValue());
 
-// function memoize(fn) {
-//   let cache = {};
+function memoize(fn) {
+  let cache = {};
 
-//   return function (arg) {
-//     if (arg in cache) {
-//       return cache[arg];
-//     }
-//     cache[arg] = fn(arg);
-//     return cache[arg];
-//   };
-// }
+  return function (arg) {
+    if (arg in cache) {
+      return cache[arg];
+    }
+    cache[arg] = fn(arg);
+    return cache[arg];
+  };
+}
 
-// // var task
+// var task
 
-// // No Error
-// x = 5;
-// console.log(x);
-// var x;
+// No Error
+x = 5;
+console.log(x);
+var x;
 
-// // Error
+// Error
 
-// y = 5;
-// console.log(y);
-// let y;
+y = 5;
+console.log(y);
+let y;
 
 // Countdown
 
@@ -82,3 +82,45 @@ function handleCountdownControl() {
 let timerID = setInterval(updateCountDown, 1000);
 
 stopButton.addEventListener("click", handleCountdownControl);
+
+// Decorator
+
+function deco(fn) {
+  return function (...arg) {
+    const start = performance.now();
+    const result = fn.call(this, ...arg);
+    const end = performance.now();
+    const duration = end - start;
+    console.log(
+      `the start time is: ${start}, The end time is ${end} the entire duration in ms is ${duration}`,
+    );
+    return result;
+  };
+}
+
+function debounce(fn, delay) {
+  let timerID;
+  return function (...args) {
+    if (timerID) {
+      clearTimeout(timerID);
+    }
+    timerID = setTimeout(() => {
+      fn.call(this, ...args);
+    }, delay);
+  };
+}
+
+function logMessage(text) {
+  console.log(text, Date.now());
+}
+
+const debouncedLog = debounce(logMessage, 300);
+setTimeout(() => {
+  debouncedLog("1");
+}, 0);
+setTimeout(() => {
+  debouncedLog("2");
+}, 500);
+setTimeout(() => {
+  debouncedLog("3");
+}, 600);
